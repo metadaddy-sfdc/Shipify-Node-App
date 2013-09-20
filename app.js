3b212199c0095cac5086a20766f489e3ff6b4a65
Raja Rao DV <rajaraodv100@yahoo.com>
2013-09-20 10:07:57 -0700

    heroku changes

--- app.js
+++ app.js
@@ -19,38 +19,41 @@ var options = {
 
 
 
-
 // Create a service (the app object is just a callback).
 var app = express();
 
 app.configure(function() {
   app.use(express.favicon());
-   app.set('view engine', 'ejs');
+  app.set('view engine', 'ejs');
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/', function(req, res){
-  res.render("index", { result: 'do post to see result here'});
+app.get('/', function(req, res) {
+  res.render("index", {
+    result: 'do post to see result here'
+  });
 });
 
 
 
-app.post('/', function(req, res){
+app.post('/', function(req, res) {
   console.log('http post');
 
-//console.log(req.body.signed_request);
+  //console.log(req.body.signed_request);
   var sfJSON;
-  try{
+  try {
     sfJSON = decode(req.body.signed_request, APP_SECRET);
-  } catch(e) {
-    res.render("error", {error: JSON.stringify(e)});
+  } catch (e) {
+    res.render("error", {
+      error: JSON.stringify(e)
+    });
     return;
   }
   res.render("index", sfJSON);
-    
+
 });
 
 
@@ -63,8 +66,10 @@ function getInvoices(req, res) {
   var q = "SELECT Id, Name, Account__c, Account__r.Name, Invoice_Total__c FROM Invoice__c";
   //console.log(q);
 
-  if(!req.headers.authorization || !req.headers.instance_url) {
-    res.json(400, {'Error': "Must pass 'instance_url' and 'Authorization' (= session_id) in the header."});
+  if (!req.headers.authorization || !req.headers.instance_url) {
+    res.json(400, {
+      'Error': "Must pass 'instance_url' and 'Authorization' (= session_id) in the header."
+    });
     return;
   }
 
@@ -92,17 +97,19 @@ function postInvoiceInfoToAccountFeed(req, res) {
   //var feedItem = nforce.createSObject('FeedItem');
   console.log(req.body);
 
-  if(!req.headers.instance_url || !req.body.ParentId || !req.headers.authorization) {
-    res.json(400, {'Error': "Must pass 'instance_url' and 'Authorization' (= session_id) in the header. Also Must pass 'ParentId' in the POST body. "})
+  if (!req.headers.instance_url || !req.body.ParentId || !req.headers.authorization) {
+    res.json(400, {
+      'Error': "Must pass 'instance_url' and 'Authorization' (= session_id) in the header. Also Must pass 'ParentId' in the POST body. "
+    })
     return;
   }
 
   /* This is a randomly generated number, but in reality would be a real number grabbed from this back end system */
-  var orderNumber = Math.floor(Math.random()*90000) + 10000;
+  var orderNumber = Math.floor(Math.random() * 90000) + 10000;
 
   var body = {
     ParentId: req.body.ParentId,
-    Body: "Invoice: " + req.body.Name + " has been shipped! Your order number is #" + orderNumber  + " " + req.headers.instance_url + "/" + req.params.invoiceId
+    Body: "Invoice: " + req.body.Name + " has been shipped! Your order number is #" + orderNumber + " " + req.headers.instance_url + "/" + req.params.invoiceId
   }
 
   var authorization = getAuthHeader(req.headers.authorization);
@@ -119,9 +126,10 @@ function postInvoiceInfoToAccountFeed(req, res) {
   console.log(options);
 
   request(options, function(err, response, body) {
-    console.log(response);
     if (!err && response.statusCode <= 201) {
-      res.json({'result': 'OK'});
+      res.json({
+        'result': 'OK'
+      });
     } else {
       res.json(400, {
         'result': 'error',
@@ -136,9 +144,13 @@ function getAuthHeader(header) {
   return h.indexOf('oauth ') == 0 ? header : 'OAuth ' + header;
 }
 
-
-
-// Create an HTTP service.
-http.createServer(app).listen(80);
-// Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(443);
+config.log("process.env.RUNNING_ON_HEROKU = " + process.env.RUNNING_ON_HEROKU);
+//if not running on Heroku..
+if (!process.env.RUNNING_ON_HEROKU) {
+  // Create an HTTP service.
+  http.createServer(app).listen(80);
+  // Create an HTTPS service identical to the HTTP service.
+  https.createServer(options, app).listen(443);
+} else {
+  http.createServer(app).listen(3000);
+}
